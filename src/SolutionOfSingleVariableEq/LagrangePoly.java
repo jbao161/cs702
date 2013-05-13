606d32e43db1b18f2e3fbb44ca2995440e991483
jbao <jbao@jbao-acer.home>
2013-05-13 16:15:07 -0400

    handles polynomial addition and multiplication, with toText() output as String

--- src/SolutionOfSingleVariableEq/LagrangePoly.java
+++ src/SolutionOfSingleVariableEq/LagrangePoly.java
@@ -4,10 +4,27 @@
  */
 package SolutionOfSingleVariableEq;
 
+import Util.Polynomial;
+
 /**
  *
  * @author jbao
  */
 public class LagrangePoly {
-    
+
+    public static void main(String[] args) {
+        double[][] py1 = {{0, -1}, {3, 1}};
+        double[][] py2 = {{0, 1}, {4, 2}};
+        Polynomial p1 = new Polynomial(py1);
+        Polynomial p2 = new Polynomial(py2);
+        Polynomial p3 = p1.times(p2);
+          System.out.println(p3.toString());
+        System.out.println(p3.toText());
+
+        Polynomial n = new Polynomial();
+
+        System.out.println(p1.add(p2).toString());
+        System.out.println(p2.add(p1).toString());
+        System.out.println(n.add(p1).toString());
+    }
 }
--- src/Util/Polynomial.java
+++ src/Util/Polynomial.java
@@ -4,17 +4,17 @@
  */
 package Util;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * key = power of variable x. value = coefficient of the variable term
  *
  * @author jbao
  */
-public class Polynomial extends HashMap<Double, Double> {
+public class Polynomial extends TreeMap<Double, Double> {
 
-    Polynomial() {
+    public Polynomial() {
     }
 
     /**
@@ -23,7 +23,7 @@ public class Polynomial extends HashMap<Double, Double> {
      *
      * @param coefs example: x^2 + 3x^4 + 2x^7 = {0,0,1,0,3,0,0,2};
      */
-    Polynomial(double[] coefs) {
+    public Polynomial(double[] coefs) {
         for (double i = 0; i < coefs.length; i++) {
             this.put(i, coefs[(int) i]);
         }
@@ -34,7 +34,7 @@ public class Polynomial extends HashMap<Double, Double> {
      *
      * @param coefs {[power, coefficient], [power, coefficient],...}
      */
-    Polynomial(double[][] coefs) {
+    public Polynomial(double[][] coefs) {
         for (int i = 0; i < coefs.length; i++) {
             this.put(coefs[i][0], coefs[i][1]);
         }
@@ -58,13 +58,16 @@ public class Polynomial extends HashMap<Double, Double> {
         Polynomial result = new Polynomial();
         double coef;
         double power;
-        for (Map.Entry<Double, Double> e : this.entrySet()) {
-            power = e.getKey();
-            coef = e.getValue();
-            if (px.containsKey(power)) {
-                coef += px.get(power);
+        Polynomial[] list = {this, px};
+        for (int i = 0; i < list.length; i++) {
+            for (Map.Entry<Double, Double> e : list[i].entrySet()) {
+                power = e.getKey();
+                coef = e.getValue();
+                if (result.containsKey(power)) {
+                    coef += result.get(power);
+                }
+                result.put(power, coef);
             }
-            result.put(power, coef);
         }
         return result;
     }
@@ -75,28 +78,76 @@ public class Polynomial extends HashMap<Double, Double> {
         double powerThis;
         double powerPx;
         Polynomial result = new Polynomial();
-        Polynomial subProduct;
+        Polynomial singleTerm;
         for (Map.Entry<Double, Double> entryThis : this.entrySet()) {
             // get the values for the term a1x^n1
             powerThis = entryThis.getKey();
             coefThis = entryThis.getValue();
-            // create a new term to store the product of a number with a polynomial
-            subProduct = new Polynomial();
-            // proceed line by line, a1x^n1 *(Px) + a2x^n2 *(Px) + ...
+            // proceed entry by entry, a1x^n1 *(b1x^n1) + a1x^n1 *(b2x^n2) + ... + a2x^n2 * (b1x^n1) + a2x^n2 *(b2x^n2) + ...
             for (Map.Entry<Double, Double> entryPx : px.entrySet()) {
+                // create a new term to store the product of a two numbers
+                singleTerm = new Polynomial();
                 // add the powers, and multiply the coefficients
                 powerPx = entryPx.getKey() + powerThis;
                 coefPx = entryPx.getValue() * coefThis;
-                // combine like terms
-                if (subProduct.containsKey(powerPx)) {
-                    coefPx += subProduct.get(powerPx);
-                }
-                // update the coefficient at the nth power
-                subProduct.put(powerPx, coefPx);
+                // set the coefficient and the power
+                singleTerm.put(powerPx, coefPx);
+                // combine the single term product into the total sum
+                result = result.add(singleTerm);
             }
-            // combine the single line product into the total sum
-            result.add(subProduct);
         }
-        return new Polynomial();
+        return result;
+    }
+
+    public double[][] toPrimitive() {
+
+        Double[][] objResult = this.entrySet().toArray(new Double[this.entrySet().size()][]);
+        double[][] result = new double[objResult.length][];
+        for (int i = 0; i < objResult.length; i++) {
+            for (int j = 0; j < objResult[i].length; j++) {
+                result[i][j] = objResult[i][j].doubleValue();
+            }
+        }
+        return result;
+    }
+
+    public String toText() {
+        String result = "";
+        String add = " + ";
+        String subtract = " - ";
+        String var = "x";
+        String exp = "^";
+        String operator;
+        for (Map.Entry<Double, Double> e : this.entrySet()) {
+            double coef = e.getValue();
+            double exponent = e.getKey();
+            // if the coefficient is negative, and the term is not a constant, use subtract String and positive coefficient
+            if (coef < 0 & exponent!=0) {
+                operator = subtract;
+                coef = -coef;
+            } else {
+                operator = add;
+            }
+
+            // don't print the term if its coefficient equals zero
+            if (coef != 0) {
+                result += operator;
+                // print coefficient if term is constant, or coefficient does not equal one
+                if (exponent == 0 || coef != 1) {
+                    result += String.valueOf(coef);
+                }
+                // print the variable name if term is not a constant
+                if (exponent != 0) {
+                    result += var;
+                }
+                // if the term is not a constant and its exponent is not one, we need to show the exponent
+                if (exponent !=0 & exponent != 1) {
+                    result += exp + e.getKey();
+                }
+            }
+        }
+        // removes the leading add string
+        result = result.substring(add.length());
+        return result;
     }
 }
