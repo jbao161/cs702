015062b933f1e996f4267b6447bdefe2c7ab1481
jbao <jbao@jbao-i5.home>
2013-10-08 05:46:25 -0400

    pj01 k7

--- src/cs780/geometry/K7_penta.java
+++ src/cs780/geometry/K7_penta.java
@@ -4,6 +4,10 @@
  */
 package cs780.geometry;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import numutil.Matrix;
 
 /**
@@ -47,10 +51,49 @@ public class K7_penta extends Project01 {
         Project01.get_distances(ap);
         System.out.println(get_potential(ap));
     }
+     public static void to_file(String filename, double[][] atom_positions) {
+        int num_atoms = atom_positions.length;
+        try {
 
+            String content = "7\r\nK7 partial\r\n";
+
+            File file = new File("/users/jbao/" + filename + ".xyz");
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            for (int i = 0; i < num_atoms; i++) {
+                int num_coordinates = atom_positions[i].length;
+                bw.write("K ");
+                for (int j = 0; j < num_coordinates; j++) {
+                    bw.write(atom_positions[i][j] + " ");
+                }
+                bw.write("\r\n");
+            }
+            bw.close();
+
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+  public static void xyz(double n) {
+        double[][] ap = p7(n);
+        to_file("k7 initial", ap);
+        minimize(ap);
+        to_file("k7 optimal", ap);
+
+    }
     public static void main(String[] args) {
         int n = 6;
         testgeometry(n);
         testminimize(n);
+         xyz(n);
     }
 }
--- src/cs780/geometry/K7_square.java
+++ src/cs780/geometry/K7_square.java
@@ -4,6 +4,10 @@
  */
 package cs780.geometry;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import numutil.Matrix;
 
 /**
@@ -29,7 +33,7 @@ public class K7_square extends Project01{
         return atom_positions;
     }
 
-    public static void testgeometry(int n) {
+    public static void testgeometry(double n) {
         double[][] ap = p7(n);
         Matrix printer;
         printer = new Matrix(ap);
@@ -37,7 +41,7 @@ public class K7_square extends Project01{
         Project01.get_distances(ap);
     }
 
-    public static void testminimize(int n) {
+    public static void testminimize(double n) {
         double[][] ap = p7(n);
         minimize(ap);
         Matrix printer;
@@ -47,9 +51,51 @@ public class K7_square extends Project01{
         System.out.println(get_potential(ap));
     }
 
+     public static void to_file(String filename, double[][] atom_positions) {
+        int num_atoms = atom_positions.length;
+        try {
+
+            String content = "7\r\nK7 partial\r\n";
+
+            File file = new File("/users/jbao/" + filename + ".xyz");
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            for (int i = 0; i < num_atoms; i++) {
+                int num_coordinates = atom_positions[i].length;
+                bw.write("K ");
+                for (int j = 0; j < num_coordinates; j++) {
+                    bw.write(atom_positions[i][j] + " ");
+                }
+                bw.write("\r\n");
+            }
+            bw.close();
+
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void xyz(double n) {
+        double[][] ap = p7(n);
+        to_file("k7 initial", ap);
+        minimize(ap);
+        to_file("k7 optimal", ap);
+
+    }
+
     public static void main(String[] args) {
-        int n = 7;
+        double n = 8;
         testgeometry(n);
         testminimize(n);
+        xyz(n);
     }
 }
