d771ec8bb2a5ad089c87e2824ddaaeb836bb0a92
jbao <jbao@jbao-i5.home>
2013-11-24 22:25:05 -0500

    almost working

---
+++ src/cs780/pj03.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs780;
+
+/**
+ *
+ * @author jbao
+ */
+public class pj03 {
+
+    static void part01(int n) {
+        double probability;
+        double pterm = num_span / num_occupied;
+    }
+
+    public static void main(String[] args) {
+        part01(10);
+    }
+}
